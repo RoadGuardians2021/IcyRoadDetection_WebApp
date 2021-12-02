cd1c7d56a2a3986617257fe8e0933bda256e67dc
TSpencer21 <49127187+TSpencer21@users.noreply.github.com>
2021-12-01 22:32:18 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
 //The marked out part is the ip address of the virtual machine and the password for the 'josh' user on phpMyAdmin
 
- $con = mysqli_connect('157.245.113.224','josh','34182057roadguardiansWSU','db'); //making the connection to the database
+ $con = mysqli_connect('34.71.49.21','ty','pizza34182057','db'); //making the connection to the database
+ 
+ //$con = mysqli_connect('34.71.49.21','josh','josh34182057','db'); //making the connection to the database
 
  if (!$con) {
      die('Connect Error:' . mysqli_connect_error()); //if connection fails, output why
@@ -13,8 +15,8 @@
  
 /******************************************************************************************************************************************************************************************************************/
 
- $temp = '25.6';
- $curr = '-0.4';
+ $temp = '35.3';
+ $curr = '0.5';
  $device_id = "Device_1";
 
  $sql = "INSERT INTO Curr_Table (current, device_id, time_now) VALUES ($curr, '$device_id', CURRENT_TIMESTAMP())"; //inserting data into table (Curr_Table) made for holding the device's current sensor data
@@ -40,7 +42,7 @@
      echo "Error: " . $sql . "<br>" . mysqli_error($con);
  }
 
-/***************************************************************************************************************************************************************************************************************/
+/***************************************************************************************************************************************************************************************************************
 
  $sql = "DELETE FROM Temp_Table WHERE time_now < DATE_SUB(NOW(), INTERVAL 1 DAY)"; //delete data from table (Temp_Table) after it has been in there for 24 hours
 
@@ -52,7 +54,7 @@
      echo "Error: " . $sql . "<br>" . mysqli_error($con);
  }
 
- /**************************************************************************************************************************************************************************************************************/
+ /**************************************************************************************************************************************************************************************************************
 
  $sql = "DELETE FROM Curr_Table WHERE time_now < DATE_SUB(NOW(), INTERVAL 1 DAY)"; //delete data from table (Curr_Table) after it has been in there for 24 hours
 
@@ -65,5 +67,31 @@
  }
 
  mysqli_close($con);
- 
+
+ /************************************************************* delete all data from Curr_Table **************************************************************************************************************************
+
+ $sql = "DELETE FROM Curr_Table"; 
+
+ if (mysqli_query($con, $sql)) {
+    echo "<br>";
+    echo "No errors";
+ }
+ else {
+     echo "Error: " . $sql . "<br>" . mysqli_error($con);
+ }
+
+/**************************************************************** delete all data from Temp_Table ******************************************************************************************************************************
+
+$sql = "DELETE FROM Temp_Table";
+
+ if (mysqli_query($con, $sql)) {
+    echo "<br>";
+    echo "No errors";
+ }
+ else {
+     echo "Error: " . $sql . "<br>" . mysqli_error($con);
+ }
+
+/**************************************************************************************************************************************************************************************************/
+
 ?>
